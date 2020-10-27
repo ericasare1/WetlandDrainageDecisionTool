e24e6551f6dbc6e1832a713b581ca2a04f55c6c0
eric <eric.asare.kojo@gmail.com>
2020-10-27 02:45:05 -0600

    updated flexdashboard layout

--- codes/new_profit.R
+++ codes/new_profit.R
@@ -1,26 +1,7 @@
 
 library(tidyverse)
 
-data <- read_csv("data/black.csv")
-data %>% dplyr::filter(crop %in% crops_selected) %>%
-  mutate(revenue = `yield(bu/acre)` * `price_fg ($/bu)`) %>% select(revenue)
-
-
-rev = data$`yield(bu/acre)`*data$`price_fg ($/bu)`
-
-column_to_rownames(data, var = "crop") %>% head()
-
-crops_selected <- c("Canola", "Corn")
-out <- data %>% dplyr::filter(crop %in% crops_selected) %>% View()
-
-data[[(crops), "yield(bu/acre)"]]
-
 #Reading in Data for specific soil zoils
-
-data <- read_csv("data/brown.csv")
-data1 <- data %>% dplyr::filter(crop %in% c("Corn", "Canola", "Flax", "Oats", "Soybean")) %>%
-  mutate(revenue = `yield(bu/acre)` * `price_fg ($/bu)`) %>% select(revenue)
-
 crop_revenue <- function(soil_zone, crops_selected = c("Corn", "Canola", "Flax", "Oats", "Soybean")) {
   
   if (soil_zone == "black") { 
@@ -28,7 +9,7 @@ crop_revenue <- function(soil_zone, crops_selected = c("Corn", "Canola", "Flax",
     data1 <- data %>% dplyr::filter(crop %in% crops_selected) %>%
       mutate(revenue = `yield(bu/acre)` * `price_fg ($/bu)`,
              cost = 2,
-             net_profit = revenue - cost) %>% select(revenue, cost, net_profit)
+             net_profit = revenue - cost) %>% select(revenue, cost, net_profit) 
     return(data1 %>% View())
   } 
   else if (soil_zone == "brown") {
@@ -51,5 +32,4 @@ crop_revenue <- function(soil_zone, crops_selected = c("Corn", "Canola", "Flax",
 }  
 
 
-
-crop_revenue("black", crops_selected = c("Corn", "Canola", "Flax"))  
+crop_revenue("brown", crops_selected = c("Corn", "Canola"))  
--- flexdashboard/rmarkdown_setup.R
+++ flexdashboard/rmarkdown_setup.R
@@ -1,21 +1,5 @@
-crop_revenue <- function(soil_zone, crops_selected = c("Corn", "Canola", "Flax", "Oats", "Soybean")) {
-  
-  if (soil_zone == "black") { 
-    data <- read_csv("data/black.csv")
-    return(data %>% View())
-  } 
-  else if (soil_zone == "brown") {
-    data <- read_csv("data/brown.csv")
-    data <- read_csv("data/black.csv")
-    return(data %>% View()) 
-  } 
-  else if (soil_zone == "darkbrown") {
-    data <- read_csv("data/darkbrown.csv")
-    data <- read_csv("data/black.csv")
-    return(data %>% View()) 
-  }
-  
-}  
 
+# If you want to generate PDF output, you will need to install LaTeX
+install.packages('tinytex') 
+tinytex::install_tinytex()  # install TinyTeX
 
-crop_revenue("black")  
